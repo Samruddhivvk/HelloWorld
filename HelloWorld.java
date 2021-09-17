ee75a0f1dc562c3123788c04f7e51375be7d0b40
Samruddhivvk <51435520+Samruddhivvk@users.noreply.github.com>
2021-09-17 10:13:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld{
+  
 public static void manin(String[] args){
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello World");
-}
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
 }
